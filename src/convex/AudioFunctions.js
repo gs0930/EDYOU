d0b0c231ae3b58186f3d2616f8b6cf12fd674589
Divya <d.prasad.8018@gmail.com>
2024-09-14 17:01:59 -0400

    Merge branch 'main' of https://github.com/gs0930/EDYOU into frontend-div

--- src/convex/AudioFunctions.js
+++ src/convex/AudioFunctions.js
@@ -1,9 +1,28 @@
 import { action } from "./_generated/server";
 
-export const doSomething = action({
-  args: {},
-  handler: async () => {
-    const data = await fetch("https://api.thirdpartyservice.com");
-    // do something with data
-  },
+export const generateSong = action({
+  args: { gptDescriptionPrompt: "string", prompt: "string", tags: "string" }, // Define the arguments needed
+  handler: async ({ gptDescriptionPrompt, prompt, tags }) => {
+    const response = await fetch("https://api.thirdpartyservice.com/api/generate/v2/", {
+      method: 'POST',
+      headers: {
+        'Authorization': `XceiWJD3LjYnOnkqErAYbQ9T740eHT4O`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        gpt_description_prompt: gptDescriptionPrompt,
+        tags: tags,
+        mv: 'chirp-v3-5'
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    
+    return data;
+  }
 });
